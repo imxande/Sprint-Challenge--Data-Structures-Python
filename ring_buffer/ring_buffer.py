2dbb09a117845d37870616719ca01255d35ea3f9
imxande <alerodriguezlarosa@gmail.com>
2020-04-24 12:40:34 -0400

    Task 1. Implement a Ring Buffer Data Structure done

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -7,14 +7,43 @@ class RingBuffer:
         self.current = None
         self.storage = DoublyLinkedList()
 
+# Method adds elements to the buffer.
     def append(self, item):
-        pass
+        # place holders
+        storage = self.storage
+        capacity = self.capacity
+        
+        # chech if storage is smaller than capacity
+        if len(storage) < capacity:
 
+        # Wraps the item and inserts it as the new tail of the list.
+            storage.add_to_tail(item)
+        else:
+            # if current or its pointer does not exist 
+            if self.current == None or self.current.next == None:
+                # we want to use the head
+                self.current = self.storage.head
+            else:
+                # otherwise we use next
+                self.current = self.current.next
+            # we want the value to be the item passed
+            self.current.value = item
+            
+# returns all of the elements in the buffer in a list in their given order
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
 
         # TODO: Your code here
+        # place holder
+        head = self.storage.head
+
+        # while head is not empty
+        while head != None:
+            # append its value to the list
+            list_buffer_contents.append(head.value)
+            # increment
+            head = head.next
 
         return list_buffer_contents
 
